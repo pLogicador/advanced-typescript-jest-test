dbb68a638a66430a1901cea106158d2e5981ba73
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-09-25 16:23:58 -0300

    feat: test Messaging.ts

---
+++ src/services/messaging.spec.ts
@@ -0,0 +1,30 @@
+import { Messaging } from "./messaging";
+
+const createSut = () => {
+  return new Messaging();
+};
+
+describe('Messaging', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    const sut = createSut();
+    expect(sut.sendMessage('teste')).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    sut.sendMessage('teste');
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Message sent:", and msg', () => {
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    sut.sendMessage('teste');
+    expect(consoleSpy).toHaveBeenCalledWith(`Message sent: ${`teste`}`);
+  });
+});
